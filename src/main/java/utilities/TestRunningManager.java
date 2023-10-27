5bc97703bf1ac3cd40d4175457c62222d492d20c
mohammedtaher95 <mohammedtaher10295@gmail.com>
2023-10-27 23:35:35 +0200

    Handling TestNG/JUnit Profiles

--- src/main/java/utilities/TestRunningManager.java
+++ src/main/java/utilities/TestRunningManager.java
@@ -1,14 +1,23 @@
 package utilities;
 
+import com.google.auto.service.processor.AutoServiceProcessor;
 import org.junit.platform.launcher.LauncherSessionListener;
 import org.testng.ITestNGListener;
 import org.testng.TestNG;
+
+import javax.imageio.spi.ServiceRegistry;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 public class TestRunningManager {
 
+    private static final Path testNG = Paths.get("target/classes/META-INF/services/org.testng.ITestNGListener");
+    private static final Path junit = Paths.get("target/classes/META-INF/services/org.junit.platform.launcher.LauncherSessionListener");
     private static boolean isJunitRunBool = false;
     private static boolean isTestNGRunBool = false;
 
@@ -22,13 +31,27 @@ public class TestRunningManager {
         identifyRunType();
 
         if (isTestNGRunBool) {
+
+            if(Files.exists(junit)){
+                try {
+                    Files.delete(junit);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             ServiceLoader<ITestNGListener> serviceLoader = ServiceLoader.load(ITestNGListener.class);
             serviceLoader.reload();
             LoggingManager.info("Start Running Tests via TestNG Runner");
         }
 
         if (isJunitRunBool) {
-
+            if(Files.exists(testNG)){
+                try {
+                    Files.delete(testNG);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
             ServiceLoader<LauncherSessionListener> serviceLoader =
                     ServiceLoader.load(LauncherSessionListener.class);
             serviceLoader.reload();
